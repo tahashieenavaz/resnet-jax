53487b1d705a77a81b6814d1e3198f8fa961be12
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-28 20:39:35 +0200

    adds main block to Resnet

--- resnet_jax/Resnet.py
+++ resnet_jax/Resnet.py
@@ -20,3 +20,12 @@ class Resnet(nn.Module):
 
         x = jax.numpy.mean(x, axis=(1, 2), keepdims=True)
         return nn.Dense(self.num_classes)
+
+
+if __name__ == "__main__":
+    model = Resnet(num_classes=10)
+    key = jax.random.PRNGKey(0)
+    data_key, init_key = jax.random.split(key)
+    dummy_input = jax.random.normal(data_key, (1, 224, 224, 3))
+    out, variables = model.init_with_output(init_key, dummy_input)
+    print("Output shape:", out.shape)
